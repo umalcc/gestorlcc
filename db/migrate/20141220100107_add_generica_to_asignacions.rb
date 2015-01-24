6d33a266005a5d6f80bce13bdae6160c5084b73d
MariaDesMeeres <mariadm.bello@gmail.com>
2015-01-24 16:59:17 +0100

    Solucionado fallo rake db:migrate

--- db/migrate/20141220100107_add_generica_to_asignacions.rb
+++
@@ -1,5 +0,0 @@
-class AddGenericaToAsignacions < ActiveRecord::Migration
-  def change
-    add_column :asignacions, :generica, :boolean
-  end
-end
--- db/migrate/20141220100555_add_generica_to_asignaciondefs.rb
+++
@@ -1,5 +0,0 @@
-class AddGenericaToAsignaciondefs < ActiveRecord::Migration
-  def change
-    add_column :asignaciondefs, :generica, :boolean
-  end
-end
