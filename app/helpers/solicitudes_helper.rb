5c8413f95500c19ad8b8988848887974e3ac2f81
MariaDesMeeres <mariadm.bello@gmail.com>
2016-10-22 18:16:15 +0200

    Fix solicitudes helper

--- app/helpers/solicitudes_helper.rb
+++ app/helpers/solicitudes_helper.rb
@@ -1,21 +1,31 @@
 module SolicitudesHelper
 	def getCurrentTeachingPeriod
 
-      primerPeriodo = Periodo.order("inicio").where("tipo = ? and cast(strftime('%m', inicio) as int) = ?",'Lectivo', 9).first
+      primerPeriodo = Periodo.order("inicio").where("iniciosol <= ? and fin >= ? and tipo = ? and cast(strftime('%m', inicio) as int) = ?",Date.today,Date.today,'Lectivo', 9).first
 
-      inicioCurso = primerPeriodo.iniciosol
-      finCurso = Date.new(inicioCurso.next_year.year,7,1)
+      if primerPeriodo != nil
+        inicioCurso = primerPeriodo.iniciosol
+        finCurso = Date.new(inicioCurso.next_year.year,7,1)
+      else
+        inicioCurso = nil
+        finCurso = nil
+      end
 	    
 	    return {:iniciocurso => inicioCurso, :fincurso => finCurso}
 	end
 
   def getCurrentTeachingPeriodForExams
 
-      primerPeriodoLectivo = Periodo.order("inicio").where("tipo = ? and cast(strftime('%m', inicio) as int) = ?",'Lectivo', 9).first
-      ultPeriodoExamenes = Periodo.order("inicio DESC").where("tipo = ? and cast(strftime('%m', inicio) as int) = ?",'Examenes', 9).first
+      primerPeriodoExamenes = Periodo.order("inicio").where("iniciosol <= ? and fin >= ? and tipo = ? and cast(strftime('%m', inicio) as int) = ?",Date.today,Date.today,'Examenes', 12).first
+      ultPeriodoExamenes = Periodo.order("inicio DESC").where("iniciosol <= ? and fin >= ? and tipo = ? and cast(strftime('%m', inicio) as int) = ?",Date.today,Date.today,'Examenes', 9).first
       
-      inicioCurso = primerPeriodoLectivo.inicio
-      finCurso = ultPeriodoExamenes.fin
+      if primerPeriodoExamenes.nil? and ultPeriodoExamenes.nil? 
+          inicioCurso = nil
+          finCurso = nil
+      else
+          inicioCurso = primerPeriodoLectivo.inicio
+          finCurso = ultPeriodoExamenes.fin
+      end
       
       return {:iniciocurso => inicioCurso, :fincurso => finCurso}
   end
@@ -85,35 +95,47 @@ module SolicitudesHelper
         periodoAcademico = getCurrentTeachingPeriodForExams
       end
 
-      return true if isValidRequest?(solicitud, periodoAcademico[:iniciocurso], periodoAcademico[:fincurso])
+      if periodoAcademico[:iniciocurso].nil? and periodoAcademico[:fincurso].nil?
+         return false
+      else
+         return isValidRequest?(solicitud, periodoAcademico[:iniciocurso], periodoAcademico[:fincurso])
+      end
   end
 
-    def isLabRequestFromLastYear?(solicitud, lectivo)
+  def isLabRequestFromLastYear?(solicitud, lectivo)
       
+      # Nota: las fechas tienen que estar actualizadas para que funcione correctamente
       if lectivo
         periodoAcademico = getCurrentTeachingPeriod
       else
         periodoAcademico = getCurrentTeachingPeriodForExams
       end
 
-      inicioCursoPasado = periodoAcademico[:iniciocurso].prev_year.year
-      finCursoPasado = periodoAcademico[:fincurso].prev_year.year
-      
-      return true if isValidRequest?(solicitud, inicioCursoPasado, finCursoPasado)
+      if periodoAcademico[:iniciocurso].nil? and periodoAcademico[:fincurso].nil?
+         return false
+      else
+        inicioCursoPasado = periodoAcademico[:iniciocurso].prev_year.year
+        finCursoPasado = periodoAcademico[:fincurso].prev_year.year
+        return isValidRequest?(solicitud, inicioCursoPasado, finCursoPasado)
+      end
     end
 
     def isLabRequestFromLast2Years?(solicitud, lectivo)
 
+      # Nota: las fechas tienen que estar actualizadas para que funcione correctamente
       if lectivo
         periodoAcademico = getCurrentTeachingPeriod
       else
         periodoAcademico = getCurrentTeachingPeriodForExams
       end
 
-      inicioCursoPasado2 = periodoAcademico[:iniciocurso].prev_year.prev_year.year
-      finCursoPasado2 = periodoAcademico[:fincurso].prev_year.prev_year.year
-      
-      return true if isValidRequest?(solicitud, inicioCursoPasado2, finCursoPasado2)    
+      if periodoAcademico[:iniciocurso].nil? and periodoAcademico[:fincurso].nil?
+         return false
+      else
+         inicioCursoPasado2 = periodoAcademico[:iniciocurso].prev_year.prev_year.year
+         finCursoPasado2 = periodoAcademico[:fincurso].prev_year.prev_year.year
+         return isValidRequest?(solicitud, inicioCursoPasado2, finCursoPasado2)   
+      end 
     end
 
     def getCurrentCuatrimesterRequests(solicitudlabs, lectivo)
